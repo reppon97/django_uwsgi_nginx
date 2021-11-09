b6410c42ba29ac060451d83d1bfb9abc8f8f0b7b
Rejep Mammedov <reppon97@gmail.com>
2021-11-09 13:41:31 +0300

    Cosmetics

--- init.sh
+++ init.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
-echo "Initializing..."
+echo -e "${CYAN}Initializing...${NC}"
 
 sudo nginx -t
 
-read -p 'Domain:' domain
+read -p "Domain:" domain
 
 sed -i "s/yourdomain/$domain/g" nginx.conf
 
 sed -i "s/yousocket/$domain\.sock/g" nginx.conf
 
-echo "Set socket name /tmp/$yourdomain.sock"
+echo -e "${GREEN}Set socket name ${YELLOW}/tmp/$domain.sock${NC}"
 
 read -p 'uWSGI Upstream name:' upstream
 
 sed -i "s/yourupstream/$upstream/g" nginx.conf
-echo "Upstream name - $upstream"
+echo -e "${GREEN}Upstream name - ${YELLOW}$upstream${NC}"
 
 mv nginx.conf nginx-$domain.conf
 
-echo "nginx-$domain.conf created"
-
-
-
+echo -e "${GREEN}Created nginx config file: ${YELLOW}nginx-${domain}.conf"
